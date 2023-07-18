fbaf49638d379669574c623e51972103b327b06f
Gabriel <ggarcia7@utmachala.edu.ec>
2023-07-17 21:42:32 -0500

    Agregar Heroe Agregado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,12 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from './components/heroe/heroe.component';
+import { NewheroComponent } from './components/newhero/newhero.component';
 
 const routes: Routes = [
-{path: 'home', component: HomeComponent}, {path: 'about', component: AboutComponent}, {path: 'heroes', component: HeroesComponent}, {path: 'heroe/:id', component: HeroeComponent}, {path: '**', pathMatch:'full', redirectTo: 'home'}
+{path: 'home', component: HomeComponent}, {path: 'about', component: AboutComponent}, {path: 'newhero', component: NewheroComponent},
+{path: 'heroes', component: HeroesComponent}, {path: 'heroe/:id', component: HeroeComponent}, 
+{path: '**', pathMatch:'full', redirectTo: 'home'}
 
 
 ];
--- src/app/components/newhero/newhero.component.ts
+++ src/app/components/newhero/newhero.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Heroe, HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-newhero',
   templateUrl: './newhero.component.html',
   styleUrls: ['./newhero.component.css']
 })
-export class NewheroComponent {
+
+
+export class NewheroComponent implements OnInit {
+  nuevoHeroe: Heroe = {
+    nombre: '',
+    bio: '',
+    img: '',
+    aparicion: '',
+    casa: ''
+  };
+
+  constructor(private heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+  }
+
+  agregarHeroe(): void {
+    // Aquí puedes agregar la lógica de validación antes de guardar el nuevo héroe
+  
+    this.heroesService.getHeroes2().push(this.nuevoHeroe);
+    this.heroesService.updateHeroes2(this.heroesService.getHeroes2());
+    // Limpia los campos después de agregar el héroe
+    this.nuevoHeroe = {
+      nombre: '',
+      bio: '',
+      img: '',
+      aparicion: '',
+      casa: ''
+    };
+  }
 
 }
+
